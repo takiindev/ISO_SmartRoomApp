7f62c870c526cbe9af695769d0834de78d9b9b0f
Takiin Nguyen <nguyenminhvu02072006@gmail.com>
2025-12-05 11:18:07 +0700

    Initial Commit

---
+++ Smart Room/Smart_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_RoomApp.swift
+//  Smart Room
+//
+//  Created by nguyenminhvu on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart RoomTests/Smart_RoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_RoomTests.swift
+//  Smart RoomTests
+//
+//  Created by nguyenminhvu on 5/12/25.
+//
+
+import Testing
+@testable import Smart_Room
+
+struct Smart_RoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
