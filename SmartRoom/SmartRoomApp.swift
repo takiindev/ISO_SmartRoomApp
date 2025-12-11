94a2d8674dd942e6f34709da576a2ebae151e760
Takiin Nguyen <nguyenminhvu02072006@gmail.com>
2025-12-11 13:18:10 +0700

    Initial Commit

---
+++ SmartRoom/SmartRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartRoomApp.swift
+//  SmartRoom
+//
+//  Created by nguyenminhvu on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
